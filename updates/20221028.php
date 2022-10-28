d10949eec278464ceecb843e371da966682b3e24
Uzume <rubywolf@fastmail.com>
2022-10-28 20:19:36 +0000

    uniqueness constraint on user id + name fields for users storage.

---
+++ updates/20221028.php
@@ -0,0 +1,21 @@
+<?php
+
+class OBUpdate20221028 extends OBUpdate
+{
+    public function items()
+    {
+        $updates   = array();
+        $updates[] = 'Add uniqueness constraint to combined user ID and name for users storage.';
+        return $updates;
+    }
+
+    public function run()
+    {
+        $this->db->query('ALTER TABLE `users_storage` ADD UNIQUE `userid_name_index`(`user_id`, `name`);');
+        if ($this->db->error()) {
+            return false;
+        }
+
+        return true;
+    }
+}
