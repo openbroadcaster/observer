6b436bdfc14a42f8f4b808f8c583c6c9c5a4b3fb
Brook Gagnon <brook@pikalabs.com>
2023-11-29 17:43:35 +0000

    language field updates (wip)

--- ui/fields/language.js
+++ ui/fields/language.js
@@ -6,96 +6,44 @@ class OBFieldLanguage extends OBField {
   // languages are common to all instances of this element
   static languages = null;
 
-  // private properties now supported with #
-  #inputLangId = null;
-  #suggestions = [];
-
   async connectedCallback() {
     if (OBFieldLanguage.languages === null) {
 
-      console.log('fetch languages');
+      console.log('getting languages');
 
       // prevent multiple calls if this element appears twice in one form
-      OBFieldLanguage.languages = [];
+      OBFieldLanguage.languages = {};
 
       const result = await OB.API.postPromise('metadata', 'language_list', {});
 
       if (!result.status) return false;
-      OBFieldLanguage.languages = result.data;
+
+      // create an object linking language "id" with language "ref_name"
+      for (const lang of result.data) {
+        OBFieldLanguage.languages[lang.language_id] = lang.ref_name;
+      }
     }
 
-    // get "value" attribute and set
-    // setting value already runs renderComponent after
-    this.value = this.getAttribute('value');
+    this.renderComponent();
   }
 
-  async renderComponent() {
-    console.log('render component');
-
-    render(html`
-      <style>
-        :host { display: inline-block; }
-
-        .wrapper {
-          display: flex;
-          flex-direction: column;
-        }
-        .suggestions
-        {
-          max-height: 200px;
-          overflow-y: auto;
-        }
-        .suggestions div {
-          cursor: default;
-          margin: 5px 0;
-        }
-      </style>
-
-      <div class="wrapper">
-        <input type="text" onInput=${this.onInput.bind(this)} />
-        <div class="suggestions">
-          ${this.#suggestions.map((lang) => html`<div data-lang=${lang.id} onClick=${() => this.selectSuggestion(lang.id)}>${lang.ref_name}</div>`)}
-        </div>
-      </div>
-    `, this.root);
-
-    this.renderEdit();
+  set value(id) {
+    console.log('setting value', id);
   }
 
-  onInput(event) {
-    const value = event.target.value;
-    if (value.length >= 2) {
-      this.#suggestions = OBFieldLanguage.languages.filter((lang) => lang.ref_name.toLowerCase().startsWith(value.toLowerCase()));
-    } else {
-      this.#suggestions = [];
+  changeEvent(e) {
+    this.value = e.target.value;
+  }
+
+  async renderEdit() {
+    render(html`<ob-field-select data-edit onChange=${this.changeEvent.bind(this)}></ob-field-select>`, this.root);
+
+    // set our field options if we don't have them already
+    this.fieldSelect = this.root.querySelector('ob-field-select');
+    if (!this.fieldSelect.options.length) {
+      this.fieldSelect.options = OBFieldLanguage.languages;
+      this.fieldSelect.refresh();
     }
-    this.refresh();
-  }
-
-  selectSuggestion(langId) {
-    this.#suggestions = [];
-    this.value = langId;
-  }
-
-  get value() {
-    return this.#inputLangId;
-  }
-
-  set value(value) {
-    const lang = OBFieldLanguage.languages.find((lang) => lang.id === value);
-
-    if (lang) {
-      // input field gets language name, but we track language ID to return when value is requested
-      this.root.querySelector('input').value = lang.ref_name;
-      this.#inputLangId = value;
-    }
-    else if (this.root.querySelector('input')) {
-      // set blank / null, language empty or not found
-      this.root.querySelector('input').value = '';
-      this.#inputLangId = null;
-    }
-
-    this.refresh();
   }
 }
 
---
+++ ui/fields/select.js
@@ -0,0 +1,233 @@
+import { html, render } from '../vendor.js';
+import { OBField } from '../base/field.js';
+
+class OBFieldSelect extends OBField {
+
+    filterVal = '';
+
+    addSelected(option) {
+        if (this.multiple && !this.value.includes(option)) {
+            // add option to selected
+            this.value.push(option);
+        }
+
+        else if (!this.multiple) {
+            this.value = option;
+        }
+
+        // update selected and blur to hide dropdown
+        this.updateSelected();
+        this.root.querySelector('#input').blur();
+    }
+
+    deleteSelected(option) {
+        // remove option from this.value
+        const index = this.value.indexOf(option);
+        if (index > -1) {
+            this.value.splice(index, 1);
+        }
+
+        this.updateSelected();
+    }
+
+    updateSelected() {
+        if (this.value === undefined || this.value === null) {
+            return;
+        }
+
+        if (this.multiple) {
+            // clear input-selected
+            this.root.querySelector('#input-selected-multiple').innerHTML = '';
+
+            // create tags for each value
+            for (const option of this.value) {
+                // create tag and add it to input-selected
+                const tag = document.createElement('span');
+                tag.classList.add('tag');
+                tag.innerHTML = this.options[option];
+
+                // onclick, delete tag
+                tag.onclick = () => {
+                    this.deleteSelected(option);
+                };
+
+                this.root.querySelector('#input-selected-multiple').appendChild(tag);
+            }
+        }
+        else {
+            this.root.querySelector('#input-selected').innerHTML = this.options[this.value] ?? '';
+        }
+
+        // emit change
+        this.dispatchEvent(new CustomEvent('change', {
+            detail: {
+                value: this.value
+            }
+        }));
+    }
+
+    filter(e) {
+        // check if key is a regular character
+        if (e.key.length === 1) {
+            this.filterVal += e.key;
+        }
+        else if (e.key === 'Backspace') {
+            this.filterVal = this.filterVal.slice(0, -1);
+        }
+
+        else return;
+
+        // display input filter
+        this.root.querySelector('#input-filter').innerHTML = this.filterVal;
+
+        // hide any options that don't contain the filter
+        const options = this.root.querySelectorAll('#options li');
+        options.forEach(option => {
+            if (option.innerHTML.toLowerCase().includes(this.filterVal.toLowerCase())) {
+                option.removeAttribute('hidden');
+            }
+            else {
+                option.setAttribute('hidden', true);
+            }
+        });
+    }
+
+    filterReset(e) {
+        this.filterVal = '';
+        this.root.querySelector('#input-filter').innerHTML = '';
+        // remove any hidden attributes
+        const options = this.root.querySelectorAll('#options li');
+        options.forEach(option => {
+            option.removeAttribute('hidden');
+        });
+    }
+
+    scss() {
+        return `
+            :host { position: relative; width: var(--field-width); display: inline-block; }
+            ul {
+                display: none;
+                list-style-type: none;
+                padding-left: 0;
+                position: absolute;
+                top: 100%;
+                left: 0;
+                border: var(--field-border);
+                z-index: 1;
+                margin-top: 0;
+                box-sizing: border-box;
+                max-height: 300px;
+                overflow: auto;
+                border-bottom-left-radius: var(--field-radius);
+                border-bottom-right-radius: var(--field-radius);
+                background: var(--field-background);
+                width: 100%;
+            }
+            ul li {
+                padding: 4px 8px;
+                cursor: pointer;
+            }
+            #input 
+            {
+                cursor: pointer;
+                width: 100%;
+                background-color: var(--field-background);
+                border: var(--field-border);
+                border-radius: var(--field-radius);
+                color: var(--field-color);
+            }
+            #input:focus 
+            {
+                border-bottom-left-radius: 0;
+                border-bottom-right-radius: 0;
+                outline: 0;
+            }
+            #input:focus ul {
+                display: block;
+            }
+
+            #input-filter {
+                // effectively hides the 
+
+            }
+
+            #input-filter:not(:empty) {
+                border-bottom: 1px solid var(--field-color);
+                margin-bottom: -1px; /* removes extra border thickness at bottom when no results */
+            }
+
+            #input-filter:hover {
+                background-color: transparent;
+                color: #000;
+            }
+
+            #input-filter:empty {
+                display: none;
+            }
+
+            /* prevent the input from collapsing when empty */
+            #input-selected:empty::after,
+            #input-selected-multiple:empty::after {
+                content: "\\00A0";
+            }
+
+            #input-selected-multiple {
+                display: flex;
+                flex-wrap: wrap;
+            }
+
+            #input-selected {
+                padding: 4px 8px;
+            }
+            .tag {
+                border: 1px solid var(--field-color);
+                border-radius: var(--field-radius);
+                padding: 2px;
+                margin: 2px;
+                position: relative;
+            }
+            .tag:hover {
+                text-decoration: line-through;
+            }`;
+    }
+
+    renderEdit() {
+        // get options from data-options and json decode
+        if (!this.options) this.options = JSON.parse(this.getAttribute('data-options'));
+        if (!this.value) this.value = JSON.parse(this.getAttribute('data-value'));
+        this.multiple = this.hasAttribute('data-multiple');
+
+        if (!this.options) this.options = {};
+        if (!this.value && this.multiple) this.value = [];
+        if (!this.value && !this.multiple) this.value = '';
+
+
+
+        render(html`
+            <div id="input" class="field" tabindex="0" onkeydown=${(e) => this.filter(e)} onblur=${(e) => this.filterReset()}>
+                ${this.multiple && html`<div id="input-selected-multiple"></div>`}
+                ${!this.multiple && html`<div id="input-selected"></div>`}
+                <ul id="options" contenteditable="false">
+                    <li id="input-filter"></li>
+                    ${!this.multiple && html`<li onClick=${() => this.addSelected('')}>(None)</li>`}
+                    ${Object.keys(this.options).map(option => html`<li onClick=${() => this.addSelected(option)}>${this.options[option]}</li>`)}
+                </ul>
+            </div>
+
+    `, this.root);
+
+        this.updateSelected();
+
+        // figure out selected and render
+        /*
+        for (const option of this.options) {
+            if (option == this.value || option.value == this.value) {
+                this.updateSelectedLabel(option);
+            }
+        }
+        */
+    }
+
+}
+
+customElements.define('ob-field-select', OBFieldSelect);
