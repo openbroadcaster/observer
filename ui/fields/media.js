7a33dec5bac44bcc6771b95f7a6065a223dae2ff
Mei Sakura <mei@eml.cc>
2024-01-10 23:42:58 +0000

    basic css for element

--- ui/fields/media.js
+++ ui/fields/media.js
@@ -8,14 +8,46 @@ class OBFieldMedia extends OBField {
 
     renderEdit() {
         render(html`
-            <div>Edit test</div>
+            <div id="media" class="media-editable"></div>
         `, this.root);
     }
 
     renderView() {
         render(html`
-            <div>View test</div>
+            <div id="media" class="media-viewable"></div>
         `, this.root);
+    }
+
+    scss() {
+        return `
+            :host {
+                #media:empty {
+                    border: 2px dashed #eeeeec;
+                }
+
+                .media-editable:empty::after {
+                    content: "Drop Media Here";
+                    display: block;
+                    text-align: center;
+                    line-height: 96px;
+                }
+
+                .media-viewable:empty::after {
+                    content: "No Media";
+                    display: block;
+                    text-align: center;
+                    line-height: 96px;
+                }
+
+                #media {
+                    box-sizing: border-box;
+                    width: 350px;
+                    max-width: 350px;
+                    min-height: 100px;
+                    display: inline-block;
+                }
+            }
+        `;
     }
 
     get value() {
