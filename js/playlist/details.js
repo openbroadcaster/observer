349601013d4c1db9617bcb77411fa189d3c187f9
Mei Sakura <mei@eml.cc>
2024-02-13 22:44:27 +0000

    remove debugging statement

--- js/playlist/details.js
+++ js/playlist/details.js
@@ -121,7 +121,6 @@ OB.Playlist.detailsPage = function(id)
       });
     }
 
-    console.log(pldata);
     if (pldata['properties'] && pldata['properties']['last_track_fadeout']) {
       $('#playlist_details_last_fadeout').text(pldata['properties']['last_track_fadeout'] + 's');
     }
