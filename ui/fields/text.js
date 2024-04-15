28ca91ca9df2c06c17c5c501ee9a7fd938a5c288
Sanae <sanae@eml.cc>
2024-04-15 19:14:51 +0000

    custom text field css fixes + allow max length

--- ui/fields/text.js
+++ ui/fields/text.js
@@ -1,7 +1,36 @@
 import { OBField } from '../base/field.js';
 
 class OBFieldText extends OBField {
+    #init;
 
+    connectedCallback() {
+        if (this.#init) {
+            return;
+        }
+
+        this.#init = true;
+        this.renderComponent().then(() => {
+            if (this.hasAttribute('maxlength')) {
+                this.root.querySelector('input').setAttribute('maxlength', this.getAttribute('maxlength'));
+            }
+        });
+    }
+
+    scss() {
+        return `
+            :host {
+                input {
+                    color: #2e3436;
+                    font-size: 13px;
+                    border-radius: 2px;
+                    border: 0;
+                    padding: 5px;
+                    width: 250px;
+                    vertical-align: middle;
+                }
+            }
+        `
+    }
 }
 
 customElements.define('ob-field-text', OBFieldText);
