45393ac62c2ec92612aa2181cc814a7ddfaab359
Sanae <sanae@eml.cc>
2024-06-04 20:25:47 +0000

    properties field to alerts table for voicetrack settings

--- updates/20240604.php
+++ updates/20240604.php
@@ -6,6 +6,7 @@ class OBUpdate20240604 extends OBUpdate
     {
         $updates = [];
         $updates[] = 'Add a mode field to the alerts table.';
+        $updates[] = 'Add a properties field to the alerts table.';
 
         return $updates;
     }
@@ -15,6 +16,12 @@ class OBUpdate20240604 extends OBUpdate
         $this->db->query("ALTER TABLE alerts ADD COLUMN mode ENUM('Interrupt', 'Voicetrack') NOT NULL AFTER frequency;");
         if ($this->db->error()) {
             echo "Failed to add mode field.";
+            return false;
+        }
+
+        $this->db->query("ALTER TABLE alerts ADD COLUMN properties TEXT AFTER mode;");
+        if ($this->db->error()) {
+            echo "Failed to add properties field.";
             return false;
         }
 
