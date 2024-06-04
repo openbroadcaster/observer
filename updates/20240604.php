466a5c5b76562c273415947d446417e29f9699ea
Sanae <sanae@eml.cc>
2024-06-04 20:37:26 +0000

    no capitals in enum

--- updates/20240604.php
+++ updates/20240604.php
@@ -13,7 +13,7 @@ class OBUpdate20240604 extends OBUpdate
 
     public function run()
     {
-        $this->db->query("ALTER TABLE alerts ADD COLUMN mode ENUM('Interrupt', 'Voicetrack') NOT NULL AFTER frequency;");
+        $this->db->query("ALTER TABLE alerts ADD COLUMN mode ENUM('interrupt', 'voicetrack') NOT NULL AFTER frequency;");
         if ($this->db->error()) {
             echo "Failed to add mode field.";
             return false;
