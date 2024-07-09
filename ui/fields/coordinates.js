f7da473525e6602671579085d39fd77bea79cdf1
Sanae <sanae@eml.cc>
2024-07-09 21:14:48 +0000

    coordinates field skeleton

---
+++ ui/fields/coordinates.js
@@ -0,0 +1,65 @@
+import { html, render } from "../vendor.js";
+import { OBField } from "../base/field.js";
+
+class OBFieldCoordinates extends OBField {
+    #init;
+
+    #value;
+
+    async connected() {
+        if (this.#init) {
+            return;
+        }
+        this.#init = true;
+
+        this.#value = "";
+
+        this.renderComponent().then(() => {});
+    }
+
+    renderEdit() {
+        render(
+            html` <input id="field" type="text" onchange=${this.#updateValue.bind(this)} value="${this.value}" /> `,
+            this.root,
+        );
+    }
+
+    renderView() {
+        render(html` <div id="field">${this.value}</div> `, this.root);
+    }
+
+    scss() {
+        return `
+            :host {
+                display: inline-block;
+
+                input {
+                    color: #2e3436;
+                    font-size: 13px;
+                    border-radius: 2px;
+                    border: 0;
+                    padding: 5px;
+                    width: 250px;
+                    vertical-align: middle;
+                }
+            }
+        `;
+    }
+
+    #updateValue(event) {
+        const value = event.target.value;
+        this.#value = value;
+        this.renderComponent();
+    }
+
+    get value() {
+        return this.#value;
+    }
+
+    set value(value) {
+        this.#value = value;
+        this.renderComponent();
+    }
+}
+
+customElements.define("ob-field-coordinates", OBFieldCoordinates);
