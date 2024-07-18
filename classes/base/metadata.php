782fa478be0e5534b3e9e3de3e983ebb4d1e4936
Sanae <sanae@eml.cc>
2024-07-18 22:25:20 +0000

    metadata field classes wip

--- classes/base/metadata.php
+++ classes/base/metadata.php
@@ -4,4 +4,7 @@ namespace OB\Classes\Base;
 
 abstract class Metadata
 {
+    public static string $sqlType;
+    public static string $constraintTable;
+    public static string $constraintColumn;
 }
--- classes/metadata/text.php
+++ classes/metadata/text.php
@@ -4,4 +4,5 @@ namespace OB\Classes\Metadata;
 
 class Text extends \OB\Classes\Base\Metadata
 {
+    public static string $sqlType = 'VARCHAR(255)';
 }
