58f3528a852f97c99c4d79762da5ddb148f5761b
Brook Gagnon <brook@pikalabs.com>
2025-04-23 17:58:46 +0000

    add unsafeCSS to vendor.js

--- ui/vendor.js
+++ ui/vendor.js
@@ -1,7 +1,7 @@
 export { html, render } from "../node_modules/htm/preact/standalone.module.js";
 export { marked } from "../node_modules/marked/lib/marked.esm.js";
 export { default as dompurify } from "../node_modules/dompurify/dist/purify.es.mjs";
-export { LitElement, html as lithtml, css as litcss } from "../extras/lit-all.min.js";
+export { LitElement, html as lithtml, css as litcss, unsafeCSS } from "../extras/lit-all.min.js";
 
 // note this uses import map already (see index.php)
 export * as sass from "sass";
