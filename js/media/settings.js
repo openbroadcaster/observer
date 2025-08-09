54b4d2b446f18cd13da6d8613b30b99b4130139c
Brook Gagnon <brook@pikalabs.com>
2025-08-09 19:33:23 +0000

    license header updates (js)

--- js/media/settings.js
+++ js/media/settings.js
@@ -1,21 +1,5 @@
-/*
-    Copyright 2012-2024 OpenBroadcaster, Inc.
-
-    This file is part of OpenBroadcaster Server.
-
-    OpenBroadcaster Server is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    OpenBroadcaster Server is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with OpenBroadcaster Server.  If not, see <http://www.gnu.org/licenses/>.
-*/
+// Copyright 2012-2024 OpenBroadcaster, Inc.
+// SPDX-License-Identifier: AGPL-3.0-or-later
 
 // get the media format settings.
 OB.Media.settings = function () {
