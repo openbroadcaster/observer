0bc325bb9296748cbc4eefeaf3f46911dc472fd1
Sanae <92342730+hinanaya@users.noreply.github.com>
2026-02-18 20:27:06 +0000

    remove assets folder from repo; fix uploads error from require

--- core/models/media_model.php
+++ core/models/media_model.php
@@ -2595,7 +2595,7 @@ class MediaModel extends OBFModel
         OBFHelpers::require_args($args, ['filename']);
         $filename = $args['filename'];
 
-        require_once('vendor/james-heinrich/getid3/getid3/getid3.php');
+        require_once(OB_LOCAL . '/vendor/james-heinrich/getid3/getid3/getid3.php');
         $getID3 = new \getID3();
 
         $info = $getID3->analyze($filename);
