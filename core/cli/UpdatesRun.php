43d7466279e596130b7d9f5860b291af2cb07316
Sanae <92342730+hinanaya@users.noreply.github.com>
2026-04-06 21:09:33 +0000

    port updates run cli

--- core/cli/UpdatesRun.php
+++ core/cli/UpdatesRun.php
@@ -2,81 +2,91 @@
 
 namespace OpenBroadcaster\CLI;
 
-if (!defined('OB_CLI')) {
-    die('Command line access only.');
-}
+use OpenBroadcaster\Base\CLI;
 
-// only run update if check passes
-Helpers::requireValid();
-
-switch ($argv[3]) {
-    case 'all':
-        echo "\033[94;1mUpdating OB Core\033[0m" . PHP_EOL;
-        runUpdates('core');
-        echo PHP_EOL . "\033[94;1mUpdating OB Modules\033[0m" . PHP_EOL;
-        runUpdates('module');
-        break;
-    case 'core':
-        runUpdates('core');
-        break;
-    case 'module':
-        runUpdates('module', $argv[4]);
-        break;
-    default:
-        throw new Exception('Unreachable switch block; update requires either all, core, or module.');
-}
-
-exit(0);
-
-function runUpdates($type = 'core', $module = null)
+class UpdatesRun extends CLI
 {
-    require_once(__DIR__ . '/../../public/updates/updates.php');
-
-    $db = new \OBFDB();
-
-    if ($type === 'core') {
-        // Run all core updates.
-        $list = $u->updates();
-    } elseif ($module !== null) {
-        $db->where('directory', $module);
-        $installed = $db->get_one('modules');
-        if (! $installed) {
-            echo "Module {$module} is not installed." . PHP_EOL;
+    public function run(array $args): bool
+    {
+        // only run update if check passes
+        if (! Helpers::requireValid()) {
             return false;
         }
 
-        // Run specified module updates.
-        $u = new \OBFUpdates($module);
-        $list = $u->updates();
-    } else {
-        // Run all module updates.
-        $modules = array_filter(scandir(__DIR__ . '/../../modules/'), fn($f) => $f[0] !== '.');
-        foreach ($modules as $module) {
-            $db->where('directory', $module);
-            $installed = $db->get_one('modules');
-            if (! $installed) {
-                continue;
-            }
-
-            echo "Running updates for module {$module}..." . PHP_EOL;
-            runUpdates('module', $module);
+        if (count($args) < 1) {
+            (new OBCLI())->help();
+            return false;
         }
 
-        return false;
+        switch ($args[0]) {
+            case 'all':
+                echo "\033[94;1mUpdating OB Core\033[0m" . PHP_EOL;
+                $this->runUpdates('core');
+                echo PHP_EOL . "\033[94;1mUpdating OB Modules\033[0m" . PHP_EOL;
+                $this->runUpdates('module');
+                break;
+            case 'core':
+                $this->runUpdates('core');
+                break;
+            case 'module':
+                $this->runUpdates('module', $args[1] ?? null);
+                break;
+            default:
+                (new OBCLI())->help();
+                return false;
+        }
+
+        return true;
     }
 
-    foreach ($list as $update) {
-        if ($update->needed) {
-            if (!$u->run($update)) {
-                echo ucwords($type) . ': Update failed, exiting.' . PHP_EOL;
-                exit(1);
+    function runUpdates($type = 'core', $module = null)
+    {
+        require_once(__DIR__ . '/../../public/updates/updates.php');
+
+        if ($type === 'core') {
+            // Run all core updates.
+            $list = $u->updates();
+        } elseif ($module !== null) {
+            $this->db->where('directory', $module);
+            $installed = $this->db->get_one('modules');
+            if (! $installed) {
+                echo "Module {$module} is not installed." . PHP_EOL;
+                return false;
             }
 
-            $prefix = "\033[94mCore:\033[0m ";
-            if ($module !== null) {
-                $prefix = "\033[94m" . implode('', array_map(fn($x) => ucwords($x), explode('_', $module))) . ":\033[0m ";
+            // Run specified module updates.
+            $u = new \OBFUpdates($module);
+            $list = $u->updates();
+        } else {
+            // Run all module updates.
+            $modules = array_filter(scandir(__DIR__ . '/../../modules/'), fn($f) => $f[0] !== '.');
+            foreach ($modules as $module) {
+                $this->db->where('directory', $module);
+                $installed = $this->db->get_one('modules');
+                if (! $installed) {
+                    continue;
+                }
+
+                echo "Running updates for module {$module}..." . PHP_EOL;
+                $this->runUpdates('module', $module);
             }
-            echo $prefix . 'Update ' . $update->version . ' installed.' . PHP_EOL;
+
+            return false;
+        }
+
+        foreach ($list as $update) {
+            if ($update->needed) {
+                if (!$u->run($update)) {
+                    echo ucwords($type) . ': Update failed, exiting.' . PHP_EOL;
+                    exit(1);
+                }
+
+                $prefix = "\033[94mCore:\033[0m ";
+                if ($module !== null) {
+                    $prefix = "\033[94m" . implode('', array_map(fn($x) => ucwords($x), explode('_', $module))) . ":\033[0m ";
+                }
+                echo $prefix . 'Update ' . $update->version . ' installed.' . PHP_EOL;
+            }
         }
     }
 }
