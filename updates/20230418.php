0be6566d96c148a6f10428218dfc672e65800949
Sanae <92342730+hinanaya@users.noreply.github.com>
2026-04-09 18:36:21 +0000

    BREAKING UPDATE BUGFIX: 20230418 playlist item properties, see description
    entire properties value was being overridden with just the query part in update file. should be fixed and now properly puts entire properties values back with new query filter. difficult to test for other tables

--- updates/20230418.php
+++ updates/20230418.php
@@ -235,6 +235,7 @@ class OBUpdate20230418 extends OBUpdate
                     $column = 'properties';
                     $properties = json_decode($row[$column], true);
                     $settings = &$properties['query'] ?? null;
+                    $fullSettings = $properties;
                 } else {
                     break;
                 }
@@ -261,7 +262,17 @@ class OBUpdate20230418 extends OBUpdate
                     continue;
                 }
 
-                $settings_encoded = ($table == 'media_searches') ? serialize($settings) : json_encode($settings);
+                if ($table === 'media_searches') {
+                    $settings_encoded = serialize($settings);
+                } elseif ($table === 'dayparting') {
+                    $settings_encoded = json_encode($settings);
+                } elseif ($table === 'playlists_items') {
+                    // Make query part of entire properties again rather than overwriting entirely.
+                    $fullSettings['query'] = $settings;
+                    $settings_encoded = json_encode($fullSettings);
+                } else {
+                    continue;
+                }
 
                 /*
                 echo 'Updating '.$table.' '.$row['id'].' '.$column."\n";
