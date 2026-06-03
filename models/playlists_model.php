13f57cf503ce53d8f20962d61de1a8e41e7b2240
Brook Gagnon <brook@pikalabs.com>
2026-06-03 19:34:17 +0000

    related to playlist resolve on remote sync, fix missing liveassist buttons when player is clone

--- models/playlists_model.php
+++ models/playlists_model.php
@@ -725,7 +725,6 @@ class PlaylistsModel extends OBFModel
             $supports = ['audio','video','image','document'];
         }
 
-
         // TODO currently no player support for documents, but will be doing this at some point.
         $support_document = $player_id ? false : true;
 
@@ -738,6 +737,20 @@ class PlaylistsModel extends OBFModel
         // get parent player
         if ($parent_player_id) {
             $parent_player = $this->models->players('get_one', $parent_player_id);
+
+            // if using parent dynamic, then we override support with that of the parent
+            if ($parent_player && $player['use_parent_dynamic']) {
+                $supports = [];
+                if ($parent_player['support_audio']) {
+                    $supports[] = 'audio';
+                }
+                if ($parent_player['support_video']) {
+                    $supports[] = 'video';
+                }
+                if ($parent_player['support_images']) {
+                    $supports[] = 'image';
+                }
+            }
         }
 
         // figure out which media IDs to exclude based on dayparting
